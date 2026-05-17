c54e277714f2c8f36b12ae74712b432ad8620e91
EG-66 <megungormus@gmail.com>
2026-05-17 19:18:21 +0200

    update url new

--- tests/web-based-authentication.spec.js
+++ tests/web-based-authentication.spec.js
@@ -4,9 +4,7 @@ test("Bypass authentication by embedding the credentials in the URL", async ({
   page,
 }) => {
   // https://username:password@practice.cydeo.com/basic_auth
-  await page.goto(
-    "https://admin:admin@https://the-internet-5chk.onrender.com/basic_auth",
-  );
+  await page.goto("https://admin:admin@the-internet-5chk.onrender.com/basic_auth");
 
   await page.waitForTimeout(3000);
 });
